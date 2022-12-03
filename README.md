4194c8d295993cabc07d5dde72075d0b47946577
Joshua Voorhies <33003771+jtvoorhies@users.noreply.github.com>
2022-12-03 15:35:56 -0600

    Added directory and files for day 02.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Advent of Code 2022
 
+Learning Python with Advent of Code.
+
+GitHub repo: jtvoorhies/advent2022
+
+## Progress
+
+- [x] #01 Calorie Counting
+- [ ] #02 Rock Paper Scissors
+- [ ] #03
+- [ ] #04
+- [ ] #05
+- [ ] #06
+- [ ] #07
+- [ ] #08
+- [ ] #09
+- [ ] #10
+- [ ] #11
+- [ ] #12
+- [ ] #13
+- [ ] #14
+- [ ] #15
+- [ ] #16
+- [ ] #17
+- [ ] #18
+- [ ] #19
+- [ ] #20
+- [ ] #21
+- [ ] #22
+- [ ] #23
+- [ ] #24
+- [ ] #25
+
---
+++ day02/day02.py
@@ -0,0 +1,51 @@
+# Advent of Code Day 2: Rock Paper Scissors
+
+from enum import Enum, auto
+
+# global variable to make functions more chatty for debugging
+verbose = True
+
+
+class InputProvider(Enum):
+    """The function getInput() is the star here, giving you the input string to
+    work with."""
+    EXAMPLE = auto()
+    INPUTFILE = auto()
+
+    def getInput(self) -> str:
+        match self:
+            case InputProvider.EXAMPLE:
+                return """\
+A Y
+B X
+C Z
+"""
+            case InputProvider.INPUTFILE:
+                inputFile = open("input.txt", mode="rt")
+                return inputFile.read()
+
+
+def run(inputProvider, part=1, expectedSolution=()):
+    print("Solving part", part, "for", inputProvider)
+    if (part < 1) or (part > 2):
+        raise ValueError("parameter 'part' must be a one or two.")
+    finishChar = "🏁"
+    solutionUnderTest = solve(inputProvider.getInput(), part=part)
+    if expectedSolution != ():
+        if expectedSolution == solutionUnderTest:
+            finishChar = "✅"
+        else:
+            finishChar = "❌"
+    print(finishChar, "Solution found:", solutionUnderTest,
+          "  expected:", str(expectedSolution), "\n")
+
+
+def solve(input, part=1):
+    # TODO Write solution
+    return -1
+
+
+run(InputProvider.EXAMPLE, part=1, expectedSolution=15)
+# run(InputProvider.INPUTFILE, part=1)
+# run(InputProvider.EXAMPLE, part=2, expectedSolution=)
+# run(InputProvider.INPUTFILE, part=2)
