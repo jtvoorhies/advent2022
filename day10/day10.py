089983f8ca5004bcf544a18ba053428dfcb46d57
Joshua Voorhies <33003771+jtvoorhies@users.noreply.github.com>
2023-04-05 03:45:23 -0500

    Solved, but not verified, part 2.

--- day10/day10.py
+++ day10/day10.py
@@ -1,9 +1,11 @@
 # Advent of Code
 
 from enum import Enum, auto
+from functools import reduce
+from itertools import repeat
 
 # global variable to make functions more chatty for debugging
-verbose = True
+verbose = False
 
 
 class InputProvider(Enum):
@@ -173,7 +175,10 @@ def run(inputProvider, part=1, expectedSolution=()):
     if (part < 1) or (part > 2):
         raise ValueError("parameter 'part' must be a one or two.")
     finishChar = "🏁"
-    solutionUnderTest = solvePart1(inputProvider.getInput())
+    solver = solvePart1
+    if part == 2:
+        solver = solvePart2
+    solutionUnderTest = solver(inputProvider.getInput())
     if expectedSolution != ():
         if expectedSolution == solutionUnderTest:
             finishChar = "✅"
@@ -183,67 +188,139 @@ def run(inputProvider, part=1, expectedSolution=()):
           "  expected:", str(expectedSolution), "\n")
 
 
-def parseInput(input: str) -> list:
-    instructions = list()
-    for line in input.splitlines():
-        if line == "noop":
-            instructions.append(None)
-        elif line.startswith("addx "):
-            num = int(line[5:])
-            instructions.append(num)
-    return instructions
-
-
-def interestingCycle(cycleCount) -> bool:
-    if cycleCount == 0:
-        return False
-    if ((cycleCount - 20) % 40) == 0:
-        return True
-    else:
-        return False
-
-
-def addx(register: int, parameter: int) -> int:
-    return register + parameter
-
-
-def noop(register: int) -> int:
-    return register
+def isInterestingCycle(n: int) -> bool:
+    return (n - 20) % 40 == 0
 
 
 def solvePart1(input) -> int:
-    rx = 1
+    splitByLine = input.splitlines()
+    regX = 1
     cycleCount = 0
     LAST_CYCLE = 220
-    cycleCountInstruction = 0
-    instructions = parseInput(input)
-    instructionPointer = 0
-    interestingRegisterValues = list()
+    instructionInputIndex = 0
+    instructionCycleCount = 0
+    instructionCyclesRequired = 1
+    signalStrengths = list()
+    instruction = lambda n: n
+    if splitByLine[0] == "noop":
+        pass
+    elif splitByLine[0].startswith("addx"):
+        parameter = int(splitByLine[0][5:])
+        instruction = lambda n: n + parameter
+        instructionCyclesRequired = 2
     while cycleCount < LAST_CYCLE:
-        if interestingCycle(cycleCount):
-            interestingRegisterValues.append(rx)
         cycleCount += 1
-        cycleCountInstruction += 1
-        currentInstruction = instructions[instructionPointer]
-        instructionRequiredCycles: int
-        currentInstructionFunction = ()
-        if currentInstruction:
-            instructionRequiredCycles = 2
-            currentInstructionFunction = addx
-        else:
-            instructionRequiredCycles = 1
-            currentInstructionFunction = noop
-        if cycleCountInstruction >= instructionRequiredCycles:
-            # apply function
-            rx = currentInstructionFunction(rx)
-            # start next instruction
-            instructionPointer += 1
-            cycleCountInstruction = 0
+        instructionCycleCount += 1
+        if isInterestingCycle(cycleCount):
+            signalStrengths.append(cycleCount * regX)
+            if verbose:
+                print("Cycle:", cycleCount)
+                print("signalStrengths:", signalStrengths)
+        if instructionCycleCount >= instructionCyclesRequired:
+            # apply instruction
+            regX = instruction(regX)
+            instructionCycleCount = 0
+            instructionInputIndex += 1
+            newInstructionString = splitByLine[instructionInputIndex]
+            if newInstructionString == "noop":
+                instruction = lambda n: n
+                instructionCyclesRequired = 1
+            elif newInstructionString.startswith("addx "):
+                parameter = int(newInstructionString[5:])
+                instruction = lambda n: n + parameter
+                instructionCyclesRequired = 2
+    output = reduce(lambda l, r: l + r, signalStrengths)
+    return output
 
+
+class Display:
+    """Python's 'str' is immutable."""
+
+    def __init__(self):
+        self.data = list(repeat(False, 240))
+
+    def strView(self) -> str:
+        '''The string representation of the display'''
+        rows = list()
+        for row in range(0, 6):
+            start = 40 * row
+            end = start + 40
+            characters = map(lambda b: "#" if b else ".", self.data[start:end])
+            s = ''.join(characters)
+            rows.append(s)
+        return "\n".join(rows)
+
+    def betterStrView(self) -> str:
+        '''The string representation of the display'''
+        rows = list()
+        for row in range(0, 6):
+            start = 40 * row
+            end = start + 40
+            characters = map(lambda b: "█" if b else " ", self.data[start:end])
+            s = ''.join(characters)
+            rows.append(s)
+        return "\n".join(rows)
+
+    def update(self, regX: int, cycle: int):
+        """Use the value of 'regX' and the current 'cycle' to update 'data'."""
+        cycle0 = cycle - 1
+        horizDrawCenter = cycle0 % 40
+        spriteRange = list([regX - 1,
+                            regX,
+                            regX + 1])
+        if horizDrawCenter in spriteRange:
+            self.data[cycle0] = True
+
+
+def solvePart2(input: str) -> str:
+    splitByLine = input.splitlines()
+    cycleCount = 0
+    regX = 1
+    display = Display()
+    instCycleCount = 0
+    instStr = splitByLine[0]
+    instCyclesNeeded = 1
+    instruction: lambda n: n
+    if instStr == "noop":
+        pass
+    elif instStr.startswith("addx"):
+        parameter = int(instStr[5:])
+        instruction = lambda n: n + parameter
+        instCyclesNeeded = 2
+    LAST_CYCLE = 240
+    instIndex = 1
+    while (cycleCount < LAST_CYCLE) and (instStr != "HALT"):
+        cycleCount += 1
+        instCycleCount += 1
+
+        # draw pixel
+        display.update(regX, cycleCount)
+
+        # check instructions
+        if instCycleCount >= instCyclesNeeded:
+            regX = instruction(regX)
+            instCycleCount = 0
+            if instIndex < len(splitByLine):
+                instStr = splitByLine[instIndex]
+            else:
+                instStr = "HALT"
+            if instStr == "noop":
+                instCyclesNeeded = 1
+                instruction = lambda n: n
+            elif instStr.startswith("addx "):
+                parameter = int(instStr[5:])
+                instCyclesNeeded = 2
+                instruction = lambda n: n + parameter
+            instIndex += 1
+    # output = display.strView()
+    output = display.betterStrView()
+    print('-------   output  -------')
+    print(output)
+    return output
 
 
 # TODO: fill in example solution
 run(InputProvider.EXAMPLE, part=1, expectedSolution=13140)
-# run(InputProvider.INPUTFILE, part=1)
-# run(InputProvider.EXAMPLE, part=2, expectedSolution=)
-# run(InputProvider.INPUTFILE, part=2)
+run(InputProvider.INPUTFILE, part=1, expectedSolution=13480)
+# run(InputProvider.EXAMPLE, part=2)
+run(InputProvider.INPUTFILE, part=2)
